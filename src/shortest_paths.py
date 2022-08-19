8022d9b8b1b228cee97eceb9cfbb09b5ac9d0330
luca <luca.bonn@alumni.fh-aachen.de>
2022-08-19 14:08:30 +0200

    Implemented bellmand ford.

--- src/shortest_paths.py
+++ src/shortest_paths.py
@@ -29,3 +29,32 @@ def dijkstra(G: Graph, start: int):
                 pred[neighbour] = node
     return 0
 
+def bellman_ford(G: Graph, start: int):
+    dist = {i:float("inf") for i in range(G.V)}
+    pred = {i:None for i in range(G.V)}
+    dist[start] = 0
+     
+    def relax(u, v):
+        c = G.adj_mat[u][v]
+        if dist[u] + c < dist[v]:
+            dist[v] = dist[u] + c
+            pred[v] = u
+            
+    for _ in range(G.V - 1):
+        for u in range(G.V):
+            for v in range(G.V):
+                if G.adj_mat[u][v] != 0:
+                    relax(u, v)
+                    
+    for u in range(G.V):
+        for v in range(G.V):
+            c = G.adj_mat[u][v]
+            if c != 0:
+                if dist[u] + c < dist[v]:
+                    print("Detected negative cycle")       
+    print(dist[0])                    
+    return 0       
+
+g = Graph(is_weighted=True, is_directed=True)
+g.build_from_txt("../sample_graphs/Wege2.txt")
+bellman_ford(g, 2)
